cdef76878fc1c4786ba6988d63f01c2d9afacdd6
Joaquim <joaquimairesleal@gmail.com>
2024-04-18 17:48:11 -0300

    Adicionando botão

--- lib/page/login_page.dart
+++ lib/page/login_page.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -20,12 +18,9 @@ class _LoginPageState extends State<LoginPage> {
 
   Widget bodyLogin() {
     return Padding(
-      padding:const EdgeInsets.all(16),
+      padding: const EdgeInsets.all(16),
       child: Column(
-        children: [
-          Image.asset("assets/logo.png"),
-          loginWidget()
-        ],
+        children: [Image.asset("assets/logo.png"), loginWidget()],
       ),
     );
   }
@@ -36,14 +31,33 @@ class _LoginPageState extends State<LoginPage> {
         const Text("Email"),
         TextFormField(
           textInputAction: TextInputAction.next,
-          decoration: const InputDecoration(border: OutlineInputBorder(), hintText: "user@email.com"),
+          decoration: const InputDecoration(
+              border: OutlineInputBorder(), hintText: "user@email.com"),
         ),
         const Text("Senha"),
         TextFormField(
           textInputAction: TextInputAction.next,
           decoration: const InputDecoration(border: OutlineInputBorder()),
         ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 12),
+          child: createButton('Entrar', () {}),
+        )
       ],
     );
   }
+}
+
+Widget createButton(String label, VoidCallback onPressed) {
+  return SizedBox(
+      height: 65,
+      width: double.infinity,
+      child: ElevatedButton(
+        onPressed: onPressed,
+        child: Text(style: TextStyle(color: Colors.black), label),
+        style: ElevatedButton.styleFrom(
+          backgroundColor: const Color.fromRGBO(39, 144, 176, 1),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+        ),
+      ));
 }
