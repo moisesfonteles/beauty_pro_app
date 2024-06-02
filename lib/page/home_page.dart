b92ae7436dec6a03c738ebf852672447b3f3b1aa
Moises Nitro 5 <moisesfonteles18@gmail.com>
2024-06-02 14:04:11 -0300

    melhorias no codigo

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,6 +1,5 @@
 
-import 'dart:ffi';
-
+import 'dart:developer';
 import 'package:beauty_pro/model/event.dart';
 import 'package:beauty_pro/page/dashboard_page.dart';
 import 'package:beauty_pro/page/edit_account.dart';
@@ -8,8 +7,6 @@ import 'package:beauty_pro/page/edit_add_service_page.dart';
 import 'package:beauty_pro/services/user_authentication.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'dart:convert';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -34,32 +31,6 @@ late String userID = _userAuthentication.getCurrentUserId() ?? "";
 
 Map<DateTime, List<Event>> _eventsMap = {};
 
-
-void getEvents () {
-  
-_userAuthentication.fetchEventsFromFirestore(userID).then((events){
-  Map<DateTime, List<Event>> _events = {};
-for (Event event in events) {
-    DateTime dateKey =  DateTime(event.date.year, event.date.month, event.date.day);
-    
-    _events.putIfAbsent(dateKey, () => []).add(event);
-  }
-  print(_events);
-  setState(() {
-  _eventsMap = _events;
-});
-});
-
-
-}
-
-List<Event> _getEventsForDay(DateTime dateKey) {
-  
-  return _eventsMap[dateKey] ?? [];
-}
-
-
-
   late CalendarFormat _calendarFormat;
   late DateTime _focusedDay;
   late DateTime _selectedDay;
@@ -71,8 +42,27 @@ List<Event> _getEventsForDay(DateTime dateKey) {
     _focusedDay = DateTime.now();
     _selectedDay = DateTime.now();
     getEvents();
-    
+  }
 
+  void getEvents () {
+    _userAuthentication.fetchEventsFromFirestore(userID).then((events){
+      Map<DateTime, List<Event>> _events = {};
+    for (Event event in events) {
+        DateTime dateKey =  DateTime(event.date.year, event.date.month, event.date.day);
+        
+        _events.putIfAbsent(dateKey, () => []).add(event);
+      }
+      log("$_events");
+      setState(() {
+      _eventsMap = _events;
+    });
+  });
+
+
+  }
+
+  List<Event> _getEventsForDay(DateTime dateKey) {
+    return _eventsMap[dateKey] ?? [];
   }
 
   @override
@@ -83,16 +73,15 @@ List<Event> _getEventsForDay(DateTime dateKey) {
         backgroundColor: const Color.fromRGBO(39, 144, 176, 1),
         child: drawerOpitions(context)
       ),
-      body:  Stack(
+      floatingActionButton: floatingActionButton(),
+      body: Column(
         children: [
-        
           TableCalendar(
-            
             eventLoader: (day) {
-                    final teste = _getEventsForDay(day);
-                    print(teste);
-                   return _getEventsForDay(day);
-                   },
+              final teste = _getEventsForDay(day);
+              print(teste);
+              return _getEventsForDay(day);
+            },
             locale: 'pt_BR',
             firstDay: DateTime.utc(2000, 1, 1),
             lastDay: DateTime.utc(2100, 1, 1),
@@ -115,190 +104,184 @@ List<Event> _getEventsForDay(DateTime dateKey) {
             },
             headerVisible: true,
           ),
-          
-       
-          Positioned(
-            bottom: 20,
-            right: 20,
-            child: FloatingActionButton(
-              onPressed: () {
-               showDialog(
-                context: context,
-                builder: (context){
-                  return AlertDialog(
-                    scrollable: true,
-                    title: const Text('Adiconar atendimento'),
-                    content: Padding(
-                      padding: const EdgeInsets.all(8),
-                      child: Column(
-                        children: [
-                          TextFormField(
-                            controller: customer,
-                                    
-                                    textInputAction: TextInputAction.next,
-                                    decoration: const InputDecoration(
-                                        labelText: "Cliente", border: OutlineInputBorder()),
-                                  ),
-                                  const SizedBox(height: 15),
-                                  TextFormField(
-                                    controller: hour,
-                                    textInputAction: TextInputAction.next,
-                                    decoration: const InputDecoration(
-                                        labelText: "Horario", border: OutlineInputBorder()),
-                                  ),
-                                  const SizedBox(height: 15),
-                                  TextFormField(
-                                    controller: service,
-                                    textInputAction: TextInputAction.next,
-                                    decoration: const InputDecoration(
-                                        labelText: "Serviço", border: OutlineInputBorder()),
-                                  ),
-                                  TextFormField(
-                                    controller: price,
-                                    textInputAction: TextInputAction.next,
-                                    decoration: const InputDecoration(
-                                        labelText: "Preço", border: OutlineInputBorder()),
-                                  ),
-                                  
-                                 
-                                 
-                        ],
-                      ),
-                      ),
-                      actions: [
-                        Center(
-                          child: createButton("Adicionar", () {
-                           if (userID != null) {
-                              _userAuthentication.addEventToUser(userID, customer.text, service.text, _selectedDay, hour.text, double.parse(price.text));
-                                 } else {
-                                    print("ID do usuário não está disponível.");
-                                  }
-                                  setState(() {
-                                    
-                                    getEvents();
-                                    
-                                  });
-                                  
-                                 
-                          Navigator.of(context).pop();
-                         }),
-                        )
-                        ,
-                        
-                        ],
-                  );
-                } );
-              },
-              child: Icon(Icons.add),
-            ),
-          ),
         ],
       ),
     );
   }
 
+  AppBar customAppBar(BuildContext context, {String title = ''}) {
+    return AppBar(
+      centerTitle: true,
+      backgroundColor: const Color.fromRGBO(39, 144, 176, 1),
+      title: Text(
+        title,
+        style: const TextStyle(color: Colors.white),
+      ),
+    );
+  }
 
-}
-
-
-AppBar customAppBar(BuildContext context, {String title = ''}) {
-  return AppBar(
-    centerTitle: true,
-    backgroundColor: const Color.fromRGBO(39, 144, 176, 1),
-    title: Text(
-      title,
-      style: const TextStyle(color: Colors.white),
-    ),
-  );
-}
-
-Widget createButton(String label, VoidCallback onPressed) {
-  return SizedBox(
-      height: 50,
-      width: 150,
-      child: ElevatedButton(
-        onPressed: onPressed,
-        style: ElevatedButton.styleFrom(
-          backgroundColor: const Color.fromRGBO(39, 144, 176, 1),
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
-        ),
-        child: Text(
-            style: const TextStyle(fontSize: 18, color: Colors.white), label),
-      ));
-}
-
-Widget drawerOpitions (BuildContext context){
-  return Padding(
-    padding: const EdgeInsets.fromLTRB(5, 128, 5, 5),
-    child: Column(
-            
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-             
-              TextButton.icon(
-                onPressed: () {
-                  Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const EditAccount()),
-                  );
-                },
-                icon: const Icon(Icons.person , size: 40, color: Colors.white),
-                label: const Text('Editar conta',
-                style: TextStyle(
-                  fontSize: 18,
-                  color: Colors.white
-                ),
-                ),
-              ),
-    
-            
-              TextButton.icon(
-                onPressed: () {
-                  Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => DashboardPage()),
-                  );
-                },
-                icon: const Icon(Icons.dashboard , size: 40, color: Colors.white),
-                label: const Text('Dashboard',
-                style: TextStyle(
-                  fontSize: 18,
-                  color: Colors.white
-                ),
-                ),
-              ),
-    
-              
-              TextButton.icon(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const EditAddServicePage()),
-                );
-                },
-                icon: const Icon(Icons.settings , size: 40, color: Colors.white),
-                label: const Text('Gerenciar serviços',
-                style: TextStyle(
-                  fontSize: 18,
-                  color: Colors.white
-                ),
-                ),
-              ),
-              
-           
-              TextButton.icon(
-                onPressed: () => UserAuthentication().logout(),
-                icon: const Icon(Icons.exit_to_app, size: 40, color: Colors.white), 
-                label: const Text('Sair',
-                style: TextStyle(
-                  fontSize: 18,
-                  color: Colors.white
-                ),
-                ),
-              ),
-            ],
+  Widget createButton(String label, VoidCallback onPressed) {
+    return SizedBox(
+        height: 50,
+        width: 150,
+        child: ElevatedButton(
+          onPressed: onPressed,
+          style: ElevatedButton.styleFrom(
+            backgroundColor: const Color.fromRGBO(39, 144, 176, 1),
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
           ),
-  );
-     
+          child: Text(
+              style: const TextStyle(fontSize: 18, color: Colors.white), label),
+        ));
+  }
+
+  Widget drawerOpitions (BuildContext context){
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(5, 128, 5, 5),
+      child: Column(
+              
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+              
+                TextButton.icon(
+                  onPressed: () {
+                    Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const EditAccount()),
+                    );
+                  },
+                  icon: const Icon(Icons.person , size: 40, color: Colors.white),
+                  label: const Text('Editar conta',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.white
+                  ),
+                  ),
+                ),
+      
+              
+                TextButton.icon(
+                  onPressed: () {
+                    Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => DashboardPage()),
+                    );
+                  },
+                  icon: const Icon(Icons.dashboard , size: 40, color: Colors.white),
+                  label: const Text('Dashboard',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.white
+                  ),
+                  ),
+                ),
+      
+                
+                TextButton.icon(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const EditAddServicePage()),
+                  );
+                  },
+                  icon: const Icon(Icons.settings , size: 40, color: Colors.white),
+                  label: const Text('Gerenciar serviços',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.white
+                  ),
+                  ),
+                ),
+                
+            
+                TextButton.icon(
+                  onPressed: () => UserAuthentication().logout(),
+                  icon: const Icon(Icons.exit_to_app, size: 40, color: Colors.white), 
+                  label: const Text('Sair',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.white
+                  ),
+                  ),
+                ),
+              ],
+            ),
+    );
+      
+  }
+
+  Widget floatingActionButton() {
+    return FloatingActionButton(
+      onPressed: () {
+        showDialog(
+        context: context,
+        builder: (context){
+          return AlertDialog(
+            scrollable: true,
+            title: const Text('Adiconar atendimento'),
+            content: Padding(
+              padding: const EdgeInsets.all(8),
+              child: Column(
+                children: [
+                  TextFormField(
+                    controller: customer,
+                            textInputAction: TextInputAction.next,
+                            decoration: const InputDecoration(
+                                labelText: "Cliente", border: OutlineInputBorder()),
+                          ),
+                          const SizedBox(height: 16),
+                          TextFormField(
+                            controller: hour,
+                            textInputAction: TextInputAction.next,
+                            decoration: const InputDecoration(
+                                labelText: "Horario", border: OutlineInputBorder()),
+                          ),
+                          const SizedBox(height: 16),
+                          TextFormField(
+                            controller: service,
+                            textInputAction: TextInputAction.next,
+                            decoration: const InputDecoration(
+                                labelText: "Serviço", border: OutlineInputBorder()),
+                          ),
+                          const SizedBox(height: 16),
+                          TextFormField(
+                            controller: price,
+                            textInputAction: TextInputAction.next,
+                            decoration: const InputDecoration(
+                                labelText: "Preço", border: OutlineInputBorder()),
+                          ),
+                ],
+              ),
+              ),
+              actions: [
+                Center(
+                  child: createButton("Adicionar", () {
+                    if (userID != null) {
+                      _userAuthentication.addEventToUser(userID, customer.text, service.text, _selectedDay, hour.text, double.parse(price.text));
+                          } else {
+                            print("ID do usuário não está disponível.");
+                          }
+                          setState(() {
+                            
+                            getEvents();
+                            
+                          });
+                          
+                          
+                  Navigator.of(context).pop();
+                  }),
+                )
+                ,
+                
+                ],
+          );
+        } );
+      },
+      child: const Icon(Icons.add),
+    );
+  }
 }
+
+
+
